8c8e9a7533a0c537c1eb0befcd4332c792164dac
John Knotts <jhnnyk@gmail.com>
2024-07-26 11:38:30 -0600

    take photo

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -11,9 +11,15 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-    </ion-content>
 
-    <ion-content :fullscreen="true">
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" v-for="photo in photos" :key="photo.filepath">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button @click="takePhoto()">
           <ion-icon :icon="camera"></ion-icon>
@@ -26,4 +32,7 @@
 <script setup lang="ts">
 import { camera, trash, close } from 'ionicons/icons';
 import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
+
+const { photos, takePhoto } = usePhotoGallery()
 </script>
